5bc9c26161568ad57abc79b49acec0d1a89befcd
Slin Castro <scastro@thoughtworks.com>
2020-03-25 18:53:37 -0500

    [#5] parse csv file

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 
 import crochet as crochet
-from flask import Flask
+from flask import Flask, request
 from scrapy.crawler import CrawlerRunner
 
+from src.extractors.CsvExtractor import CsvExtractor
 from src.extractors.NationalCovidSpider import NationalCovidSpider
 from src.repository.NationalRepository import NationalRepository
 
@@ -12,12 +13,21 @@ app = Flask(__name__)
 crawl_runner = CrawlerRunner()
 
 
-@app.route('/Ecuador')
+@app.route('/Ecuador/live')
 def national_statistic():
     scrape_with_crochet()
     data = NationalRepository.get_national_data()
 
     return data
+
+
+@app.route('/Ecuador')
+def data_filtering():
+    by = request.args.get('by')
+    value = request.args.get('equal')
+    filter = {"column": by,
+              "value": value}
+    return CsvExtractor().get_data_by(filter)
 
 
 @app.route('/load')
---
+++ src/extractors/CsvExtractor.py
@@ -0,0 +1,27 @@
+# Load the Pandas libraries with alias 'pd'
+import json
+
+import pandas as pd
+
+class CsvExtractor:
+    data = pd.read_csv("covid_ec.csv")
+
+    def get_data_by(self, filter):
+        column = filter["column"]
+        value = filter["value"]
+
+        province = self.data.loc[self.data[column] == value]
+        print(province)
+        province_dict = province.to_dict(orient='records')
+        province_json = json.dumps(province_dict)
+        print(province_json)
+        return province_json
+
+extractor = CsvExtractor()
+
+parameters = {
+  "column": "nombre_provincia",
+  "value": "pichincha"
+}
+
+extractor.get_data_by(parameters)
