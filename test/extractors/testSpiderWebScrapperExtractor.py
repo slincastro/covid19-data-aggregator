ac8526b4766b447b49e057b6e4e251a9c3e12ace
Slin Castro <scastro@thoughtworks.com>
2020-03-26 23:10:40 -0500

    #6 include test for parse data

--- test/extractors/testSpiderWebScrapperExtractor.py
+++ test/extractors/testSpiderWebScrapperExtractor.py
@@ -1,11 +1,31 @@
 import codecs
+import json
+
 from src.extractors.SpiderWebScrapperExtractor import SpiderWebScrapperExtractor
 from scrapy.http import HtmlResponse
 
 
-def test_data_extractor():
-    file = codecs.open("test/resources/covid_stub.html", 'r')
-    response = HtmlResponse(url="my HTML string", body=file.read(), encoding='utf-8')
+def test_when_extract_html_file_give_me_4_values():
+    response = load_html()
     data = SpiderWebScrapperExtractor().extract_data(response)
     assert data == ['1,965', '1,403', '3', '34']
 
+
+def test_when_parse_data_extractor_should_write_json_file():
+    response = load_html()
+    SpiderWebScrapperExtractor().parse(response)
+    current_data = get_data()
+    current_data["time"] = 0
+    assert current_data == {"suspicious": "1,965", "confirmed": "1,403", "recoveries": "3", "deaths": "34", "time": 0}
+
+
+def load_html():
+    file = codecs.open("test/resources/covid_stub.html", 'r')
+    response = HtmlResponse(url="my HTML string", body=file.read(), encoding='utf-8')
+    return response
+
+
+def get_data():
+    with open('data.txt') as json_file:
+        data = json.load(json_file)
+    return data
