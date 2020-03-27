7ce2f79aa449461231772bc2915f347c69f71dda
Slin Castro <scastro@thoughtworks.com>
2020-03-26 23:12:42 -0500

    #6 refactor code

--- test/extractors/testSpiderWebScrapperExtractor.py
+++ test/extractors/testSpiderWebScrapperExtractor.py
@@ -8,7 +8,8 @@ from scrapy.http import HtmlResponse
 def test_when_extract_html_file_give_me_4_values():
     response = load_html()
     data = SpiderWebScrapperExtractor().extract_data(response)
-    assert data == ['1,965', '1,403', '3', '34']
+    expect_data = ['1,965', '1,403', '3', '34']
+    assert data == expect_data
 
 
 def test_when_parse_data_extractor_should_write_json_file():
@@ -16,7 +17,8 @@ def test_when_parse_data_extractor_should_write_json_file():
     SpiderWebScrapperExtractor().parse(response)
     current_data = get_data()
     current_data["time"] = 0
-    assert current_data == {"suspicious": "1,965", "confirmed": "1,403", "recoveries": "3", "deaths": "34", "time": 0}
+    expected_data = {"suspicious": "1,965", "confirmed": "1,403", "recoveries": "3", "deaths": "34", "time": 0}
+    assert current_data == expected_data
 
 
 def load_html():
